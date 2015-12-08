3ac93af4696eab1be3c274def07a574e0074ba8a
Peter Širka <petersirka@gmail.com>
2015-12-08 08:01:06 +0100

    Updated minification.

--- minify.sh
+++ minify.sh
@@ -1,2 +1,4 @@
 ECHO "[COMPILING]"
-uglifyjs jrouting.js -c -m -o jrouting.min.js
+# uglifyjs jrouting.js -c -m -o jrouting.min.js
+uglifyjs jrouting.js -c -o jrouting.min.js
+total --minify jrouting.js
